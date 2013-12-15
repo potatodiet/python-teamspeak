842a37ef449c7c2028d2c391dc1546476bf2c132
Justin Harrison <pyrohail.ca@gmail.com>
2013-12-15 02:03:42 -0800

    Fixing up testing

--- run_tests.py
+++ run_tests.py
@@ -3,7 +3,7 @@ import teamspeak3
 teamspeak = teamspeak3.teamspeak3()
 teamspeak.connect()
 
-teamspeak.command('login', {'client_login_name': 'serveradmin', 'client_login_password': 'K7Y8PqyE'})
+teamspeak.command('login', {'client_login_name': 'serveradmin', 'client_login_password': 'travis_test'})
 teamspeak.command('use', {'sid': '1'})
 
 teamspeak.command('hostinfo')
