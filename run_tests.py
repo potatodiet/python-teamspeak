ea6d9d8efad8c6cf19ef4de4cdef157e44c3a8b2
Justin Harrison <pyrohail.ca@gmail.com>
2013-12-15 18:16:40 -0800

    Testing travis

--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,5 @@
 import teamspeak3
+import time
 
 teamspeak = teamspeak3.teamspeak3()
 teamspeak.connect()
@@ -7,3 +8,6 @@ teamspeak.command('login', {'client_login_name': 'serveradmin', 'client_login_pa
 teamspeak.command('use', {'sid': '1'})
 
 teamspeak.command('hostinfo')
+
+time.sleep(5)
+teamspeak.command('exit')
