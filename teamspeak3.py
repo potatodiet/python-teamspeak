3dacbd44736f9543d52b1236c303941e751c91bb
Justin Harrison <pyrohail.ca@gmail.com>
2013-12-15 21:36:34 -0800

    Once again lowered buffer size

--- teamspeak3.py
+++ teamspeak3.py
@@ -10,7 +10,7 @@ class teamspeak3:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.sock.connect((self.host, self.port))
 
-            first_message = self.sock.recv(8).decode('utf-8').strip()
+            first_message = self.sock.recv(4).decode('utf-8').strip()
 
             if first_message != 'TS3':
                 print('This connection is not responding as a TeamSpeak 3 server.')
